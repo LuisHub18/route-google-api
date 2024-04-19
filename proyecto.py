bc7577553c68904b531074a2b3556c506752f5ab
Sebastian <seb02tian20@gmail.com>
2024-04-19 09:52:04 -0700

    Ultimos cmabios

--- proyecto.py
+++ proyecto.py
@@ -1,8 +1,45 @@
 import copy
 import csv
+import os
 import random
 from typing import Optional
 from random import randint
+from PIL import Image
+
+from dotenv import load_dotenv
+import googlemaps
+import googlemaps.maps
+import googlemaps.distance_matrix
+import googlemaps.directions
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+gmaps = googlemaps.Client(key=API_KEY)
+
+points = {
+    "forum": (24.814255766993092, -107.40069025553083),
+    "explanada": (24.746813970159995, -107.44599053226524),
+    "tec_culiacan": (24.789105722097062, -107.39670416109986),
+    "ceiba": (24.79848761268018, -107.4231976034287),
+    "catedral": (24.808844787599703, -107.39406046109939),
+    "estadio_tomateros": (24.798925888956187, -107.38997589414312),
+    "parque_riberas": (24.8130087000168, -107.3891793072115),
+    "parque_87": (24.773593786354102, -107.38861731743529),
+    "parque_acuatico": (24.80812955949714, -107.40794481027028),
+    "plaza_sendero": (24.825444175827357, -107.42706774393118),
+    "zoo_culiacan": (24.814883270179752, -107.38424246468543),
+    "aeropuerto": (24.767827561065104, -107.47018750115168),
+    "splash_club": (24.73110314693567, -107.34740119877841),
+    "plaza_san_isidro": (24.755952183680453, -107.40270133061294),
+    "jardin_botanico": (24.827146880138784, -107.3859378744943),
+    "plaza_pabellon": (24.80403532723677, -107.34387579458584),
+    "lomita": (24.789922976167656, -107.393827599695),
+    "hospital_general": (24.794560572980007, -107.38695941812759),
+    "imss": (24.79741997857549, -107.39011115045139),
+    "plaza_ventura": (24.813700381413142, -107.40864841582747),
+}
 
 
 class Point:
@@ -46,13 +83,13 @@ def main():
     destination = 'lomita'
     alll.remove(destination)
 
-    poblation_size = 1500
-    generations = 1000
+    poblation_size = 150
+    generations = 100
     poblation = generate_tables(copy.copy(alll), poblation_size, destination)
     zzz = []
     best_of_all = None
     for generation in range(generations):
-        # print(f"Generation: {generation + 1}")
+        print(f"Generation: {generation + 1}")
         # print("5 Muestras")
         # print(f"{poblation[:5]}...")
         candidates = selection(poblation, 2, distances)
@@ -71,6 +108,19 @@ def main():
             best_of_all = best
     print(best_of_all)
     print(fitness(best_of_all, distances))
+
+    points_route = []
+    for point in best_of_all:
+        points_route.append(points[point])
+    print(points_route)
+
+    with (open('ruta.png', 'wb') as f):
+        path = googlemaps.maps.StaticMapPath(points=points_route)
+        for chunk in googlemaps.maps.static_map(gmaps, size=(1000, 1000), zoom=12, path=path, maptype="roadmap"):
+            if chunk:
+                f.write(chunk)
+    img = Image.open('ruta.png')
+    img.show()
 
 
 def selection(poblation: list[list[str]], fathers_to_select: int, distances: list[Distance]) -> list[list[str]]:
